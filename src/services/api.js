23c0d9d72e9a04d7cff77d2ad402836a8fa6b487
allanvini <allanvinisilva@gmail.com>
2020-01-21 01:12:41 -0300

    Mobile App of DevRadar application

---
+++ src/services/api.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'exp://s5-jkn.anonymous.mobile.exp.direct:80',
+})
+
+export default api;
+
+//continua no video Desenvolvendo o App Mobile aos 1:19:52
